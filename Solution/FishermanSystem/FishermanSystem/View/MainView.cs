69d37662ddfc486700b4e394d9a803d3c0611151
hristo-darlyanov <83130557+hristo-darlyanov@users.noreply.github.com>
2023-05-08 19:52:41 +0300

    Added Biggest catch from hobby fishers query

--- Solution/FishermanSystem/FishermanSystem/View/MainView.cs
+++ Solution/FishermanSystem/FishermanSystem/View/MainView.cs
@@ -22,16 +22,17 @@ namespace FishermanSystem.View
 
         private void btnShowExpiringLicenses_Click(object sender, EventArgs e)
         {
-            var ds = controller.CustomQuery("select boat_name, expire_date from [Boat] where expire_date BETWEEN GETDATE() AND DATEADD(month, 1, GETDATE())");
+            var ds = controller.CustomQuery("select boat_name, expire_date from [Boat] where expire_date between GETDATE() and DATEADD(month, 1, GETDATE())");
             dgvQueryData.ReadOnly = true;
             dgvQueryData.DataSource = ds.Tables[0];
         }
 
         private void btnShowBiggestCatchFromHobbyFishers_Click(object sender, EventArgs e)
         {
-            // To do
+            var ds = controller.CustomQuery("select u.username, SUM(f.amount_fished_kg) as amount_fished_for_the_last_year from [User] as u inner join [Fishing_Session] as f on (u.register_id = f.register_id) where date_of_fishing_session between GETDATE() and DATEADD(year, 1, GETDATE()) and u.hobby = 1 group by u.username, f.amount_fished_kg order by f.amount_fished_kg");
+            dgvQueryData.ReadOnly = true;
+            dgvQueryData.DataSource = ds.Tables[0];
         }
-
 
         private void btnShowFishingInfo_Click(object sender, EventArgs e)
         {
